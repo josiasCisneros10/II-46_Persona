c2b51f5eeee8462a372f8c80f735bffda30d31c6
JosiasCisneros <morenojosiascisneros@gmail.com>
2025-09-23 19:54:48 -0600

    Add project files.

---
+++ Default.aspx.vb
@@ -0,0 +1,9 @@
+﻿Public Class _Default
+    Inherits Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        Dim p1 As New Persona()
+        Dim p2 As New Persona(25)
+        Dim p3 As New Persona("Juan", "Perez", 25)
+    End Sub
+End Class
---
+++ Models/Persona.vb
@@ -0,0 +1,47 @@
+﻿Public Class Persona
+    Private nombre As String
+    Private apellido As String
+    Private edad As Integer
+
+    Public Property Nombre1 As String
+        Get
+            Return nombre
+        End Get
+        Set(value As String)
+            nombre = value
+        End Set
+    End Property
+
+    Public Property Apellido1 As String
+        Get
+            Return apellido
+        End Get
+        Set(value As String)
+            apellido = value
+        End Set
+    End Property
+
+    Public Property Edad1 As Integer
+        Get
+            Return edad
+        End Get
+        Set(value As Integer)
+            edad = value
+        End Set
+    End Property
+
+    Public Sub New()
+        'Constructor por defecto
+        Me.nombre = "No hay nombre"
+    End Sub
+
+    Public Sub New(edad As Integer)
+        Me.edad = edad
+    End Sub
+
+    Public Sub New(nombre1 As String, apellido1 As String, edad1 As Integer)
+        Me.Nombre1 = nombre1
+        Me.Apellido1 = apellido1
+        Me.Edad1 = edad1
+    End Sub
+End Class
